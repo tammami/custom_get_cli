33c9d7c240418a5d6affef022ec6fca0807f45b5
ThinkFree <t4m215@gmail.com>
2024-03-04 11:36:05 +0800

    fix : fix create generate app_route recase

--- lib/functions/routes/get_add_route.dart
+++ lib/functions/routes/get_add_route.dart
@@ -61,7 +61,7 @@ String _pathsToRoute(List<String> pathSplit) {
   var sb = StringBuffer();
   for (var e in pathSplit) {
     sb.write('_Paths.');
-    sb.write(e.snakeCase.toUpperCase());
+    sb.write(e.lowerCamelCase);
     if (e != pathSplit.last) {
       sb.write(' + ');
     }
